65e195fc88aa61dd4a2876e1150691e29205b64e
jefferson <jeffepok@gmail.com>
2021-03-30 11:56:00 +0000

    Added maternal medical history model

---
+++ models/MaternalMedicalHistory.js
@@ -0,0 +1,12 @@
+const {pregnantWomanSchema} = require('./PregnantWoman');
+
+const maternalMedicalHistorySchema = new mongoose.Schema({
+    hypertension: Boolean,
+    heartDisease: Boolean,
+    sickle_cell: Boolean,
+    allergies: String,
+    pregnantWoman: pregnantWomanSchema
+});
+
+module.exports.maternalMedicalHistorySchema = maternalMedicalHistorySchema;
+module.exports.MaternalMedicalHistory = mongoose.model('MaternalMedicalHistory', maternalMedicalHistorySchema);
